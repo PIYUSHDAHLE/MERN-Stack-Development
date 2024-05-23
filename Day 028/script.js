6e5e291ba66626f99d38abd63d808474ece4c7a5
PIYUSHDAHLE <piyushdahle@gmail.com>
2024-05-23 09:28:55 +0530

    OPERATOR

---
+++ Day 028/script.js
@@ -0,0 +1,33 @@
+let a=55,b=44;
+
+//artimatic
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+//shorhand
+let n1=10;
+
+//both are having the same result
+// n1=n1+40;
+n1+=40;
+
+console.log( " n1 : "+n1);
+
+// ++ --
+let s=500;
+
+s1=s++;
+console.log(s1);
+console.log(s);
+console.log(s++);
+console.log(s);
+console.log(++s);
+console.log(s);
+console.log(s--);
+console.log(s);
+console.log(--s);
+console.log(s);
