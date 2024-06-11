16e6dcbf31161bc7969c9a2af0f95db054e65a6e
PIYUSHDAHLE <piyushdahle@gmail.com>
2024-06-11 15:17:17 +0530

    Practice of js

---
+++ Day 034/prepinsta.js
@@ -0,0 +1,55 @@
+// #1 WAP to find the Positive or Negative number 
+
+// let n=0;
+
+// if (n<0) {
+//      console.log(`the number is negative`);
+//      } else if (n>0) {
+//          console.log(`the number is Positive`);
+//          }else{
+//              console.log(`the number is zero`);
+// }
+
+// #2 WAP to find the Even or Odd number
+
+// let n=33;
+// if (n%2==0) {
+    //     console.log(`${n} is even number`);
+    //     } else {
+        //         console.log(`${n} is odd number`);
+        // }
+        
+// #3 WAP to Sum of First N Natural numbers
+
+// let n=10,sum=0;
+//  for (let i = 1; i <= n; i++) {
+    //      sum=sum+i;
+    //      }
+    
+    //      console.log(`The sum of ${n} natural number is ${sum}`)
+    
+    
+// #4 WAP to Sum of the number if no. is even then sum only even natural numbers else no. is odd then sum all the odd number 
+
+// let num = 5,sum=0;
+
+// if (num%2===0) {
+//     for (let i = 0; i <= num; i++) {
+//         if (i%2===0) {
+//             sum+=i;
+//         }    }
+//         console.log(`the sum of the ${num} even natural number is ${sum}`);
+// } else {
+//     for (let i = 0; i <= num; i++) {
+//         if (i%2!==0) {
+//             sum+=i;
+//         }    }
+//         console.log(`the sum of the ${num} odd natural number is ${sum}`);
+// }
+
+// #5 Sum of numbers in a given range
+// let firstNum,lastNum=6,sum=0;
+// for(firstNum= 5;firstNum<=lastNum;firstNum++){
+//     sum+=firstNum;  
+//     }
+// console.log(` Sum of numbers in a given range of( ${firstNum} and ${lastNum} ) is ${sum}`);
