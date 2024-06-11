ecb23d738158cf2f94862afd533ca06d428e45ed
PIYUSHDAHLE <piyushdahle@gmail.com>
2024-06-11 14:27:54 +0530

    Practice of js

---
+++ Day 034/practice.js
@@ -0,0 +1,48 @@
+// 8. WAP to accept an int and check even or odd number
+// let n=3;
+// if (n%2==0) {
+//     console.log(`${n} is even number`);
+//     } else {
+//         console.log(`${n} is odd number`);
+// }
+
+//9. WAP to accept 3 int and check amount them weather
+// i. all no are equal
+// ii. any 2 are equal
+// iii. all are different
+// and print suitable message accoring to it
+
+// let n1 = 20, n2=10, n3=10;
+
+// if (n1===n2 && n2===n3 && n1===n3) {
+//     console.log(`All the numbers are equal`);
+//     }
+// else if (n1===n2 || n2===n3 || n1===n2 )  {
+//         console.log(` the two numbers are equal`);
+// } else {
+//   console.log('All the numbers are different');  
+// }
+
+
+// 10. i.WAP to accept 2 int and check the greater no.
+//     ii.WAP to accept 3 int and check the greater no.
+
+// 1st one
+// let n1 = 20,n2=50;
+// if(n1>n2){
+//     console.log(`${n1} is greater then ${n2}`);
+//     }else{
+//     console.log(`${n2} is greater then ${n1}`);
+// }
+
+
+// 2nd one
+
+let n1 = 2000,n2=500 ,n3=70;
+if(n1>n2 && n1>n3){
+    console.log(`${n1} is greater then ${n2} and ${n3}`);
+    }else if(n2>n1 && n2>n3){
+    console.log(`${n2} is greater then ${n1} and ${n3}`);
+    }else{
+    console.log(`${n3} is greater then ${n1} and ${n2}`);
+}
