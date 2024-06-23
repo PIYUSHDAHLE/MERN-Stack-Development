8508c8d43fa39b598e94f4e53898457c6bfca2e3
PIYUSHDAHLE <piyushdahle@gmail.com>
2024-06-23 20:30:10 +0530

    update

---
+++ Day 026 (JS)/script.js
@@ -0,0 +1,8 @@
+// PROCESS OF CREATING Variable
+
+// 1. var/let/const are keywords
+// 2. Variable/container name
+// 3. =, assignmental operator, right(value) to left(Variable)
+// 4. value/info/data
+
+  let a = 15 ; 
