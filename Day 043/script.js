efc9052bb21c15af65a0306bbddb036fcc095152
PIYUSHDAHLE <piyushdahle@gmail.com>
2024-06-24 09:21:41 +0530

    ARRAY METHODS

---
+++ Day 043/script.js
@@ -0,0 +1,44 @@
+// slice method 
+let arr = [11,45,52,46,74,5,10,44];
+// arr.slice(startingIndex,endingIndex +1);
+let x =arr.slice(2,-2);
+console.log(x);
+console.log(arr);
+
+// slice are immutable because it's not change the main data it's  only change the copy DATA
+
+// splice are mutable because it's change the main data
+
+//Difference traverse technique
+
+//way 1
+// for (let index = 0; index < array.length; index++) {
+//    console.log(index,arr[arr]);
+// }
+
+//way 2(forEach)
+
+// arr.forEach((element,index)=>{
+//     console.log(index,element);
+// });
+
+
+//map method
+
+let w = arr.map((element,index)=>{
+    return (element,index);
+});
+
+console.log(w);
+
+//filter method
+
+let q = arr.filter((element,index)=>{
+    return element%2!==0;
+});
+
+console.log(q);
+console.log(arr);
+
+//JSON (JAVASCRIPT OF NOTATION)
+[{},{},{}]
