d5216685db44e38a1b470d11a6fefd6066075971
PIYUSHDAHLE <piyushdahle@gmail.com>
2024-06-25 08:55:03 +0530

    OBJECT BASICS

--- Day 044 [Object]/script.js
+++ Day 044 [Object]/script.js
@@ -1,6 +1,7 @@
 //Object 
-// An object in JavaScript is a collection of key-value pairs, where each key (a string) maps to a value of any type, used to store data and methods.
+// An object is a collection of key-value pairs, where each key (a string) maps to a value of any type, used to store data and methods.
 
+//create
 let detail ={
     name:"John",
     age:30,
@@ -8,7 +9,20 @@ let detail ={
     gender:"male"
 }
 
+//read
 console.log(detail);
 console.log(detail.name);
 console.log(detail["age"]);
 console.log(typeof detail); //object
+
+
+//update
+detail.name ="Piyush";
+
+//add
+detail["email"]="piyushdahle@gmail.com";
+console.log(detail);
+
+//delete
+delete detail.age;
+console.log(detail);
