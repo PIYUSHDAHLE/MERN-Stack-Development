e57c5a8fe31057bcb5bdfd6a97e0028e9f0e2ff5
PIYUSHDAHLE <piyushdahle@gmail.com>
2024-07-03 19:04:28 +0530

    EventListener Que

---
+++ Day 055[Que]/script.js
@@ -0,0 +1,22 @@
+const name = prompt("Enter your name : ")
+
+const h1 = document.querySelector("h1");
+
+h1.innerHTML = `Hello, ${name}`;
+
+// console.log(h1.innerHTML);
+
+////////////////////////////////////////////
+
+const box = document.querySelector(".box");
+const h2 = document.querySelector("h2");
+
+h2.innerHTML ='hello boss';
+
+box.appendChild(h2);
+
+/////////////////////////////////////
+
+window.addEventListener("load",()=>{
+    console.log("Hello");
+})
