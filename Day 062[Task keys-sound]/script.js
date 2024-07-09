b969fad219befec18226f3ee09a7e6f9e8777a32
PIYUSHDAHLE <piyushdahle@gmail.com>
2024-07-09 18:00:08 +0530

    Task : keys with sounds

---
+++ Day 062[Task keys-sound]/script.js
@@ -0,0 +1,16 @@
+const body = document.querySelector("body");
+
+body.addEventListener("keydown", (e) => {
+    const element = document.querySelector(`.${e.key}`);
+    element.style.backgroundColor = "white";
+    element.style.color = "black";
+    element.children[0].play();
+});
+
+body.addEventListener("keyup", (e) => {
+    const element = document.querySelector(`.${e.key}`);
+    element.style.backgroundColor = "black";
+    element.style.color = "white";
+    element.children[0].pause();
+    // element.children[0].load();
+});
