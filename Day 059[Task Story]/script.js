0d938105b235eda16445c19e71a358250e968160
PIYUSHDAHLE <piyushdahle@gmail.com>
2024-07-10 08:46:38 +0530

    Task : Story

--- Day 059[Task Story]/script.js
+++ Day 059[Task Story]/script.js
@@ -4,6 +4,7 @@ const storiesjson = [
         profile:
             "https://images.unsplash.com/photo-1709136494437-5b3e29bfe62b?q=80&w=2596&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
+    
     {
         dp: "https://plus.unsplash.com/premium_photo-1673757121102-0ca51260861f?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         profile:
